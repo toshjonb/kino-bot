f65b587a05a4f409e2ea3d722be6699e2cf71e93
toshjonb <toshjonbe@gmail.com>
2026-03-15 09:54:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,32 +8,27 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-async def check_sub(user_id):
-
-    member = await bot.get_chat_member(CHANNEL, user_id)
-
-    if member.status in ["member","administrator","creator"]:
-        return True
-
-    return False
-
-
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
 
-    if not await check_sub(message.from_user.id):
+    await message.answer("🎬 Kino bot")
 
-        await message.answer(
-            f"❗ Kanalga obuna bo‘ling\n\n{CHANNEL}"
-        )
+
+# ADMIN PANEL
+@dp.message_handler(commands=['admin'])
+async def admin(message: types.Message):
+
+    if message.from_user.id != ADMIN:
         return
 
-    await message.answer(
-        "🎬 Kino bot\n\nKino nomi yoki kodini yozing"
-    )
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    kb.add("➕ Kino qo‘shish")
+
+    await message.answer("👑 Admin panel", reply_markup=kb)
 
 
-# kino ID bilan
+# kino ID
 @dp.message_handler(lambda message: message.text.isdigit())
 async def movie_code(message: types.Message):
 
@@ -50,10 +45,8 @@ async def movie_code(message: types.Message):
         caption=movie[4]
     )
 
-    await add_view(movie[0])
 
-
-# kino nomi bilan qidiruv
+# SEARCH (ENG OXIRIDA BO‘LISHI SHART)
 @dp.message_handler()
 async def search(message: types.Message):
 
@@ -68,46 +61,8 @@ async def search(message: types.Message):
 
         await message.answer_video(
             m[3],
-            caption=f"""
-🎬 {m[1]}
-
-📂 {m[2]}
-⭐ {m[5]} ko‘rildi
-ID: {m[0]}
-"""
+            caption=f"{m[1]}\nID: {m[0]}"
         )
 
 
-# admin panel
-@dp.message_handler(commands=['admin'])
-async def admin(message: types.Message):
-
-    if message.from_user.id != ADMIN:
-        return
-
-    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-
-    kb.add("➕ Kino qo‘shish")
-
-    await message.answer(
-        "👑 Admin panel",
-        reply_markup=kb
-    )
-
-
-# kino qo‘shish
-@dp.message_handler(lambda message: message.text == "➕ Kino qo‘shish")
-async def add_movie_start(message: types.Message):
-
-    if message.from_user.id != ADMIN:
-        return
-
-    await message.answer("🎬 Kino nomini yuboring")
-
-
-async def on_startup(dp):
-
-    await create_db()
-
-
-executor.start_polling(dp, on_startup=on_startup)
+executor.start_polling(dp)
